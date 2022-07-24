93c889206c43c44ae0eb6bfb0f786d04a1051ac7
faseelmo <65114418+faseelmo@users.noreply.github.com>
2022-07-25 03:14:20 +0530

    initial commit
    build folder

---
+++ build/conv_pkg/CMakeFiles/cpp_conv_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_conv_node.dir/src/conversion_node.cpp.o"
+  "cpp_conv_node"
+  "cpp_conv_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_conv_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
